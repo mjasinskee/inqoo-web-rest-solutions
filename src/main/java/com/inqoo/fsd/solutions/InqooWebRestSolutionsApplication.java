25f4e2cb0996615ee765473628238ba798ba4044
marcin jasinski <m.jasinskee@gmail.com>
2022-06-04 15:38:59 +0200

    init

---
+++ src/main/java/com/inqoo/fsd/solutions/InqooWebRestSolutionsApplication.java
@@ -0,0 +1,13 @@
+package com.inqoo.fsd.solutions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InqooWebRestSolutionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InqooWebRestSolutionsApplication.class, args);
+    }
+
+}
